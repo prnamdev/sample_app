0f304c06adba980daf0d99cd53cfd2b2480422fd
prnamdev <prnamdev@bestpeers.com>
2022-05-09 18:28:43 +0530

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -24,6 +24,12 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'faker',  '2.1.2'
+
+gem 'will_paginate', '3.1.8'
+
+gem 'bootstrap-will_paginate', '1.0.0'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,10 @@
 module SessionsHelper
-  # Logs in the given user.
+# Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # Remembers a user in a persistent session.
+# Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
@@ -25,6 +25,11 @@ module SessionsHelper
     end
   end
 
+# Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+
 # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
@@ -37,11 +42,23 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
 # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-end
+  end
+
+# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+# Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
 
