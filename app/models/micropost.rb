04d2bc5311587d61c0b11e7d2e2b3daff2b65dde
prnamdev <prnamdev@bestpeers.com>
2022-05-13 18:54:17 +0530

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,9 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
-                                    message: "must be a valid image format" },
-                    size:           { less_than: 5.megabytes,
-                                    message: "should be less than 5MB" }
+    message: "must be a valid image format" },
+                    size: { less_than: 5.megabytes, message:"should be less than 5MB" }
+
 
   # Returns a resized image for display.
   def display_image
